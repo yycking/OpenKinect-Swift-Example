81bc04a9970a42d8690bbfa7235ab8dc84fc2276
Wayne Yeh <wayne.yeh@eten.com.tw>
2017-11-01 13:40:27 +0800

    Initial Commit

---
+++ OpenKinect-Swift-Example/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  OpenKinect-Swift-Example
+//
+//  Created by Wayne Yeh on 2017/11/1.
+//  Copyright © 2017年 Wayne Yeh. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
